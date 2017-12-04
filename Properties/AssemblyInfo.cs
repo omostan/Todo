1530b699e1bcb7ff2ee9cd0d3a94d87fca0a4b58
Stan Omoregie <omostan@users.noreply.github.com>
2017-12-04 15:07:53 +0100

    - removed unnecessary references and move the project up one folder level

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using Todo.Properties;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CustomAssemblyInfo")]
+[assembly: AssemblyDescription("Todo List")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("OMOTECH")]
+[assembly: AssemblyCustomAuthor("Stanley Omoregie")]
+[assembly: AssemblyCustomDepartment("IT-AM")]
+[assembly: AssemblyProduct("CustomAssemblyInfo")]
+[assembly: AssemblyCopyright("Copyright © omotech 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9dc8441c-e3f7-4062-89b4-de9ad8e8d34a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
